f11f1919844de8398201e1a8ba7231fe22107e1f
nirmeet-baweja <nirmeet.baweja@gmail.com>
2023-05-24 23:46:22 +0100

    installed tailwindcss & added navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <header>
+      <Navbar />
       <h1>Nirmeet's portfolio</h1>
       <img src="/logo192.png" alt="" />
     </header>
