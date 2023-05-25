445588f8575c7b54ff856bc14115da6ba099abcf
nirmeet-baweja <nirmeet.baweja@gmail.com>
2023-05-25 14:06:08 +0100

    added projects section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <div class="min-h-screen">
       <Navbar />
       <Hero />
+      <Projects />
       <Footer />
     </div>
   );
