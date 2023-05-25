7fa85574003b11628ad55b2d55eb84c7d2dae415
nirmeet-baweja <nirmeet.baweja@gmail.com>
2023-05-25 11:45:12 +0100

    added the hero section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <header>
       <Navbar />
+      <Hero />
       <h1>Nirmeet's portfolio</h1>
       <img src="/logo192.png" alt="" />
     </header>
