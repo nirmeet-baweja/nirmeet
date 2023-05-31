aced28956d45db5507e3167c4724565f0836f6ba
nirmeet-baweja <nirmeet.baweja@gmail.com>
2023-05-31 15:37:27 +0100

    added european countries to phone number field

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -126,31 +126,78 @@ export default function Contact() {
             >
               Phone number
             </label>
-            <div className="relative mt-2.5">
-              <div className="absolute inset-y-0 left-0 flex items-center">
+            <div className="flex flex-col md:flex-row">
+              <div className="w-full">
                 <label htmlFor="country" className="sr-only">
                   Country
                 </label>
                 <select
                   id="country"
                   name="country"
-                  className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                  className="block w-full rounded-l-lg border-0 px-4.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 text-ellipsis overflow-hidden"
                 >
-                  <option>US</option>
-                  <option>CA</option>
-                  <option>EU</option>
+                  <option value="358">{`Aland Islands +358`}</option>
+                  <option value="355">{`Albania +355`}</option>
+                  <option value="376">{`Andorra +376`}</option>
+                  <option value="43">{`Austria +43`}</option>
+                  <option value="375">{`Belarus +375`}</option>
+                  <option value="32">{`Belgium +32`}</option>
+                  <option value="387">{`Bosnia and Herzegovina +387`}</option>
+                  <option value="359">{`Bulgaria +359`}</option>
+                  <option value="385">{`Croatia +385`}</option>
+                  <option value="420">{`Czech Republic +420`}</option>
+                  <option value="45">{`Denmark +45`}</option>
+                  <option value="372">{`Estonia +372`}</option>
+                  <option value="298">{`Faroe Islands +298`}</option>
+                  <option value="358">{`Finland +358`}</option>
+                  <option value="33">{`France +33`}</option>
+                  <option value="49">{`Germany +49`}</option>
+                  <option value="350">{`Gibraltar +350`}</option>
+                  <option value="30">{`Greece +30`}</option>
+                  <option value="44">{`Guernsey +44`}</option>
+                  <option value="39">{`Holy See (Vatican City State) +39`}</option>
+                  <option value="36">{`Hungary +36`}</option>
+                  <option value="354">{`Iceland +354`}</option>
+                  <option value="353">{`Ireland +353`}</option>
+                  <option value="44">{`Isle of Man +44`}</option>
+                  <option value="39">{`Italy +39`}</option>
+                  <option value="44">{`Jersey +44`}</option>
+                  <option value="383">{`Kosovo +383`}</option>
+                  <option value="371">{`Latvia +371`}</option>
+                  <option value="423">{`Liechtenstein +423`}</option>
+                  <option value="370">{`Lithuania +370`}</option>
+                  <option value="352">{`Luxembourg +352`}</option>
+                  <option value="389">
+                    {`Macedonia, the Former Yugoslav Republic of +389`}
+                  </option>
+                  <option value="356">{`Malta +356`}</option>
+                  <option value="373">{`Moldova, Republic of +373`}</option>
+                  <option value="377">{`Monaco +377`}</option>
+                  <option value="382">{`Montenegro +382`}</option>
+                  <option value="31">{`Netherlands +31`}</option>
+                  <option value="47">{`Norway +47`}</option>
+                  <option value="48">{`Poland +48`}</option>
+                  <option value="351">{`Portugal +351`}</option>
+                  <option value="40">{`Romania +40`}</option>
+                  <option value="378">{`San Marino +378`}</option>
+                  <option value="381">{`Serbia +381`}</option>
+                  <option value="381">{`Serbia and Montenegro +381`}</option>
+                  <option value="421">{`Slovakia +421`}</option>
+                  <option value="386">{`Slovenia +386`}</option>
+                  <option value="34">{`Spain +34`}</option>
+                  <option value="47">{`Svalbard and Jan Mayen +47`}</option>
+                  <option value="46">{`Sweden +46`}</option>
+                  <option value="41">{`Switzerland +41`}</option>
+                  <option value="380">{`Ukraine +380`}</option>
+                  <option value="44" selected>{`United Kingdom +44`}</option>
                 </select>
-                <ChevronDownIcon
-                  className="pointer-events-none absolute right-3 top-0 h-full w-5 text-gray-400"
-                  aria-hidden="true"
-                />
               </div>
               <input
                 type="tel"
                 name="phone-number"
                 id="phone-number"
                 autoComplete="tel"
-                className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-r-lg border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
