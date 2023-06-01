eebd7ff136fc0b5390a859dd387f1de85410621a
nirmeet-baweja <nirmeet.baweja@gmail.com>
2023-06-01 22:21:10 +0100

    update contact element heading

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -35,7 +35,7 @@ export default function Contact() {
       </div>
       <div className="mx-auto max-w-2xl text-center">
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-          Contact sales
+          Contact
         </h2>
         <p className="mt-2 text-lg leading-8 text-gray-600">
           Aute magna irure deserunt veniam aliqua magna enim voluptate.
