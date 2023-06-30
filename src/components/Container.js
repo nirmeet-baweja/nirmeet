ab0985ea86f152ea0c30b0201d4b35a046a530e6
nirmeet-baweja <73722230+nirmeet-baweja@users.noreply.github.com>
2023-06-30 16:56:48 +0100

    Container styling

--- src/components/Container.js
+++ src/components/Container.js
@@ -5,7 +5,7 @@ const Container = ({ id, heading, children }) => {
     <div className="bg-white py-24 sm:py-32" id={id}>
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         {heading && (
-          <div className="mx-auto max-w-2xl lg:mx-0 border-b-gray-200">
+          <div className="mx-auto lg:mx-0 pb-4 sm:pb-8 border-b border-gray-200">
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
               {heading}
             </h2>
