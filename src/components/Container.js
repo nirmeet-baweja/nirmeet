1ad6252f47a05ae02fbbd22bbf324efb769d6c7a
nirmeet-baweja <73722230+nirmeet-baweja@users.noreply.github.com>
2023-06-30 15:28:17 +0100

    add Container component

---
+++ src/components/Container.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Container = ({ id, heading, children }) => {
+  return (
+    <div className="bg-white py-24 sm:py-32" id={id}>
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        {heading && (
+          <div className="mx-auto max-w-2xl lg:mx-0 border-b-gray-200">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              {heading}
+            </h2>
+          </div>
+        )}
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Container;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+      },
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/forms")],
