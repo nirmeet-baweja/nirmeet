0b91154517e678b0c870f39412ce57f402b9ec87
nirmeet-baweja <73722230+nirmeet-baweja@users.noreply.github.com>
2023-06-30 17:23:04 +0100

    moved projects data to json file

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,49 +1,6 @@
 import Container from "./Container";
 import Project from "./Project";
-
-const projects = [
-  {
-    id: 1,
-    title: "Say Your Pain",
-    href: "https://nirmeet-baweja.github.io/say-your-pain/",
-    description:
-      "An app that translates different pain sensations into multiple other languages to enable are non-native english speakers to describe their pain.",
-    imageUrl: "/assets/projects/say-your-pain.png",
-  },
-  {
-    id: 1,
-    title: "Sudoku Solver",
-    href: "https://nirmeet-baweja.github.io/sudoku-solver/",
-    description:
-      "If you ever get stuck at a game of sudoku you can use this app to solve it. The algorithm uses recursion to find the solution.",
-    imageUrl: "/assets/projects/sudoku-solver.png",
-  },
-  {
-    id: 1,
-    title: "TV Show Library",
-    href: "https://nirmeet-baweja.github.io/tv-show-dom-project/",
-    description:
-      "Get all the information about favourite TV shows at one place. A project build on DOM manipulation in vanilla JavaScript. The TV show data is fetched from a REST API.",
-    imageUrl: "/assets/projects/tv-show-dom-project.png",
-  },
-  {
-    id: 1,
-    title: "Attendance Tracker Application",
-    href: "https://github.com/nirmeet-baweja/access-denied",
-    description:
-      "A full stack application built for Code Your Future to enable volunteers to track trainees' attendance and attentiveness in class.",
-    imageUrl: "/assets/projects/access-denied.png",
-  },
-  {
-    id: 1,
-    title: "Coding Session Sign-up",
-    href: "https://github.com/nirmeet-baweja/capgemini-homework-club-backend",
-    description:
-      "An application to provide a common platform between Capgemini Volunteers and Code Your Future (CYF) trainees to sign-up for coding sessions. This app has three types of users - Admin, Volunteer and Student. Admins have the responsibility to set-up the coding sessions and assign the topics for the session. Volunteers join the session to provide their expertise and help students with the doubts they are facing. Students sign-up to sessions to get help from volunteers with their ongoing projects, doubts or classes.",
-    imageUrl: "/assets/projects/homework-club.png",
-  },
-  // More projects...
-];
+import projects from "../data/projects.json";
 
 export default function Projects() {
   return (
