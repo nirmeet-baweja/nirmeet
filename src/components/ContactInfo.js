b062da20de6e05ea340c13c094a51fe561571bc3
nirmeet-baweja <73722230+nirmeet-baweja@users.noreply.github.com>
2023-06-30 17:23:56 +0100

    moved contact info data to separate js file

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -1,29 +1,14 @@
 import React from "react";
 import Container from "./Container";
 import ProfileCard from "./ProfileCard";
-import LinkedIn from "./LinkedIn";
-import Github from "./Github";
+import { contactProfiles } from "../data/contactProfiles";
 
 const ContactInfo = () => {
-  const linkedinProfile = {
-    name: "Nirmeet Baweja",
-    username: "@nirmeet-baweja",
-    title: "Software developer 👩🏽‍💻",
-    link: LinkedIn,
-  };
-
-  const githubProfile = {
-    name: "Nirmeet Baweja",
-    username: "@nirmeet-baweja",
-    title: "Software developer 👩🏽‍💻",
-    link: Github,
-  };
-
   return (
     <Container id="contact" heading="Contact Information">
       <div className="flex flex-row justify-evenly items-stretch flex-wrap pt-10 sm:pt-16 w-full">
-        <ProfileCard profile={linkedinProfile} />
-        <ProfileCard profile={githubProfile} />
+        <ProfileCard profile={contactProfiles.linkedinProfile} />
+        <ProfileCard profile={contactProfiles.githubProfile} />
       </div>
     </Container>
   );
---
+++ src/data/contactProfiles.js
@@ -0,0 +1,17 @@
+import LinkedIn from "../components/LinkedIn";
+import Github from "../components/Github";
+
+export const contactProfiles = {
+  linkedinProfile: {
+    name: "Nirmeet Baweja",
+    username: "@nirmeet-baweja",
+    title: "Software developer 👩🏽‍💻",
+    link: LinkedIn,
+  },
+  githubProfile: {
+    name: "Nirmeet Baweja",
+    username: "@nirmeet-baweja",
+    title: "Software developer 👩🏽‍💻",
+    link: Github,
+  },
+};
