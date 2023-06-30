f0c4df965b3e1142680b1ab24c238fb30d17a2e4
nirmeet-baweja <73722230+nirmeet-baweja@users.noreply.github.com>
2023-06-30 16:56:31 +0100

    update ContactInfo to use Container

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Container from "./Container";
 import ProfileCard from "./ProfileCard";
 import LinkedIn from "./LinkedIn";
 import Github from "./Github";
@@ -19,22 +20,12 @@ const ContactInfo = () => {
   };
 
   return (
-    <div
-      className="flex flex-col justify-center items-stretch bg-indigo-100/70 min-h-max p-16 mb-16 "
-      id="contact"
-    >
-      <div className="lg:w-3/5 max-w-7xl mx-auto">
-        <div className="mx-auto max-w-2xl my-12 lg:mx-0 p-4 ">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Contact Information
-          </h2>
-        </div>
-        <div className="flex flex-row justify-evenly items-stretch flex-wrap w-full">
-          <ProfileCard profile={linkedinProfile} />
-          <ProfileCard profile={githubProfile} />
-        </div>
+    <Container id="contact" heading="Contact Information">
+      <div className="flex flex-row justify-evenly items-stretch flex-wrap pt-10 sm:pt-16 w-full">
+        <ProfileCard profile={linkedinProfile} />
+        <ProfileCard profile={githubProfile} />
       </div>
-    </div>
+    </Container>
   );
 };
 
