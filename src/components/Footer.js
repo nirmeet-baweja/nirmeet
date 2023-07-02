c6bc861d20fd545e9964b8ad828f665a28bd93fc
nirmeet-baweja <73722230+nirmeet-baweja@users.noreply.github.com>
2023-07-02 13:18:23 +0100

    update Footer to use IconLink

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,15 @@
-import Github from "./Github";
-import LinkedIn from "./LinkedIn";
+import GithubLogo from "./GithubLogo";
+import LinkedInLogo from "./LinkedInLogo";
+import IconLink from "./IconLink";
 
 export default function Footer() {
   return (
     <div className="fixed bottom-0 w-screen z-50 bg-white flex justify-center">
-      <LinkedIn />
-      <Github />
+      <IconLink
+        url="https://www.linkedin.com/in/nirmeet-baweja"
+        icon={LinkedInLogo}
+      />
+      <IconLink url="https://github.com/nirmeet-baweja" icon={GithubLogo} />
     </div>
   );
 }
